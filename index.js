85dffb26010a9607b3873b566d157acef774e803
NathanM <nathan.milburn@outlook.com>
2022-02-27 20:01:38 -0800

    did more formatting for my html template and created BASIC framework for the manager class

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const generateHTML = ({ name, github, linkedin}) =>
+const generateHTML = ({ name, position, idNumber, officeNumber, email, github}) =>
 `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,19 +9,33 @@ const generateHTML = ({ name, github, linkedin}) =>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://kit.fontawesome.com/825c894de9.js" crossorigin="anonymous"></script>
     <title>Employee Profiles</title>
 </head>
-<body>
-    <div class="jumbotron jumbotron-fluid">
-        <div class="container">
-          <h1 class="display-4">Employee Name: ${name}</h1>
-          <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-          <ul class="list-group">
-            <li class="list-group-item">My GitHub username is ${github}</li>
-            <li class="list-group-item">LinkedIn: ${linkedin}</li>
-          </ul>
+<header>
+    <div class="shadow-lg jumbotron jumbotron-fluid w-100">
+        <div>
+            <h1 class="display-4">Our Employees</h1>
         </div>
     </div>
+</header>
+<body>
+    <div class="row w-100">
+    <div class="col=12 card">
+        <div class="card-header">
+            <h2 class="card-title">${name}</h2>
+            <h2 class="card-title"><i class="fas fa-glasses mr-2"></i>${position}</h2>
+        </div>    
+        <div class="card-body">
+                <ul class="list-group">
+                <li class="list-group-item">ID: ${idNumber}</li>
+                <li class="list-group-item">Office Number: ${officeNumber}</li>
+                <li class="list-group-item">Email: ${email}</li>
+                <li class="list-group-item">GitHub: ${github}</li>
+                </ul>
+            </div>
+    </div>
+</div> 
 </body>
 </html>`;
 
@@ -36,17 +50,3 @@ inquirer
       err ? console.log(err) : console.log('Employee Profile Page Created!')
       );
     });
-
-    // Test Template for html //
-/* <div id="employeeList" class="container mt-4">
-    <div class="row">
-        <div class="col col-md-4 card bg-primary text-white Employee1" style="width: 18rem;">
-            <div class="card-body">
-                <h4 id="Name" class="card-title Employee1"></h4>
-            </div>
-        <ul class="list-group list-group-flush Stats">
-            <li class="list-group-item Temp" id="name"></li>
-            <li class="list-group-item Wind" id="Wind"></li>
-            <li class="list-group-item Humidity" id="Humidity"></li>
-        </ul>
-    </div> */
