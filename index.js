d71d1812f80e0118752d9795f2ad7fe9eff842dd
NathanM <nathan.milburn@outlook.com>
2022-03-04 14:16:56 -0800

    added beginning manager function as well as add team member function

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const inquirer = require('inquirer');
-const fs = require('fs');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const generateHTML = ({ name, role, idNumber, officeNumber, email, github}) =>
-`<!DOCTYPE html>
+const generateHTML = ({ name, role, idNumber, officeNumber, email, github }) =>
+  `<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -42,14 +42,73 @@ const generateHTML = ({ name, role, idNumber, officeNumber, email, github}) =>
 </body>
 </html>`;
 
-inquirer
-    .prompt([
-    
-    ])
-    .then((answers) => {
-      const generateHTMLPage = generateHTML(answers)
+// inquirer
+//     .prompt([
 
-      fs.writeFile('index.html', generateHTMLPage, (err) => 
-      err ? console.log(err) : console.log('Employee Profile Page Created!')
+//     ])
+//     .then((answers) => {
+//       const generateHTMLPage = generateHTML(answers)
+
+//       fs.writeFile('index.html', generateHTMLPage, (err) =>
+//       err ? console.log(err) : console.log('Employee Profile Page Created!')
+//       );
+//     });
+
+function startGenerator() {
+  managerInfo();
+}
+
+function managerInfo() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the full name of the team manager?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the team manager's employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the team manager's email address?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the team manager's office number?",
+      },
+    ])
+    .then((val) => {
+      const manager = new Manager(
+        val.name,
+        val.id,
+        val.email,
+        val.officeNumber
       );
     });
+}
+
+function addTeamMember() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "what_type",
+        message: "Add an engineer or intern to the team?",
+        choices: ["Engineer", "Intern", "No more members to add"],
+      },
+    ])
+    .then((val) => {
+      if (val.what_type === "Engineer") {
+        engineerInfo();
+      } else if (val.what_type === "Intern") {
+        internInfo();
+      } else {
+        createDocument();
+      }
+    });
+}
