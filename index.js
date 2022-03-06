6b9a10f8e9b43e302da4a745383eac1e76592434
NathanM <nathan.milburn@outlook.com>
2022-03-06 15:30:52 -0800

    added some new files and deleted test code to clean up js file

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// const Employee = require("./lib/Employee");
+const path = require("path");
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "ourteam.html");
+
+const render = require("./lib/createhtml");
 
 const teamMembers = [];
 
 function startGenerator() {
   managerInfo();
-  generateHtml();
 }
 
 function managerInfo() {
@@ -43,31 +47,9 @@ function managerInfo() {
         val.email,
         val.officeNumber
       );
+      console.log(manager);
       teamMembers.push(manager);
-      // updateHTML();
       addMembers();
-    });
-}
-
-function addMembers() {
-  inquirer
-    .prompt([
-      {
-        type: "list",
-        name: "what_type",
-        message: "Would you like to add an engineer or intern to the team?",
-        choices: ["Engineer", "Intern", "No more members to add"],
-      },
-    ])
-    .then((val) => {
-      if (val.what_type === "Engineer") {
-        engineerInfo();
-      } else if (val.what_type === "Intern") {
-        internInfo();
-      } else {
-        // updateHTML();
-        finishHtml();
-      }
     });
 }
 
@@ -96,9 +78,14 @@ function engineerInfo() {
       },
     ])
     .then((val) => {
-      const engineer = new Engineer(val.name, val.id, val.email, val.github);
+      const engineer = new Engineer(
+      val.name, 
+      val.id, 
+      val.email, 
+      val.github
+      );
+      console.log(engineer);
       teamMembers.push(engineer);
-      // updateHTML();
       addMembers();
     });
 }
@@ -128,103 +115,44 @@ function internInfo() {
       },
     ])
     .then((val) => {
-      const intern = new Intern(val.name, val.id, val.email, val.school);
+      const intern = new Intern(
+      val.name, 
+      val.id, 
+      val.email, 
+      val.school
+      );
       teamMembers.push(intern);
-      // updateHTML();
+      console.log(intern);
       addMembers();
     });
 }
 
-function generateHtml() {
-  const html = `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-      <title>Team Profile</title>
-  </head>
-  <body>
-    <div class="shadow-lg jumbotron jumbotron-fluid w-100">
-      <div class="d-flex justify-content-center container">
-        <h1 class="display-4">Our Employees</h1>
-      </div>
-    </div>
-      <div class="container">
-          <div class="row">`;
-  fs.writeFile("ourTeam.html", html, function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
+function addMembers() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "select_role",
+        message: "Would you like to add an engineer or intern to the team?",
+        choices: ["Engineer", "Intern", "No more members to add"],
+      },
+    ])
+    .then((val) => {
+      if (val.select_role === "Engineer") {
+        engineerInfo();
+      } else if (val.select_role === "Intern") {
+        internInfo();
+      } else {
+        finishHtml();
+      }
+    });
 }
 
 function finishHtml() {
-  const html = ` </div>
-  </div>
-  
-</body>
-</html>`;
-
-  fs.appendFile("ourTeam.html", html, function (err) {
-    if (err) {
-      console.log(err);
-    }
-  });
-  console.log("end");
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(teamMembers), "UTF-8");
 }
-
 startGenerator();
 
-// Test function for appending to the html file //
-// function updateHTML(member) {
-//   const name = member.getName();
-//   const role = member.getRole();
-//   const id = member.getId();
-//   const email = member.getEmail();
-//   let data = "";
-//   if (role === "Manager") {
-//     const officeNumber = member.getOfficeNumber();
-//     data = `<div class="col-6">
-//             <div class="card mx-auto mb-3" style="width: 18rem">
-//             <h5 class="card-header">${name}<br /><br />Manager</h5>
-//             <ul class="list-group list-group-flush">
-//                 <li class="list-group-item">Employee ID: ${id}</li>
-//                 <li class="list-group-item">Email: ${email}</li>
-//                 <li class="list-group-item">Office #: ${officeNumber}</li>
-//             </ul>
-//             </div>
-//         </div>`;
-//   } else if (role === "Engineer") {
-//     const gitHub = member.getGithub();
-//     data = `<div class="col-6">
-//       <div class="card mx-auto mb-3" style="width: 18rem">
-//       <h5 class="card-header">${name}<br /><br />Engineer</h5>
-//       <ul class="list-group list-group-flush">
-//           <li class="list-group-item">ID: ${id}</li>
-//           <li class="list-group-item">Email Address: ${email}</li>
-//           <li class="list-group-item">GitHub: ${gitHub}</li>
-//       </ul>
-//       </div>
-//   </div>`;
-//   } else {
-//     const school = member.getSchool();
-//     data = `<div class="col-6">
-//             <div class="card mx-auto mb-3" style="width: 18rem">
-//             <h5 class="card-header">${name}<br /><br />Intern</h5>
-//             <ul class="list-group list-group-flush">
-//                 <li class="list-group-item">ID: ${id}</li>
-//                 <li class="list-group-item">Email Address: ${email}</li>
-//                 <li class="list-group-item">School: ${school}</li>
-//             </ul>
-//             </div>
-//         </div>`;
-//   }
-//   console.log("adding team member");
-//   fs.appendFile("ourTeam.html", data, function (err) {
-//     if (err) {
-//       console.log(err);
-//     }
-//   });
-// }
